70d9a0182b891a98886557bd6b11886b0245123e
Goran Marić <45515666+GocaMaric@users.noreply.github.com>
2024-01-16 23:10:38 +0100

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -2,33 +2,27 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
-class LoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={
-        'placeholder': 'Your username',
-        'class': 'w-full py-4 px-6 rounded-xl'
-    }))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Your password',
-        'class': 'w-full py-4 px-6 rounded-xl'
-    }))
-class SignupForm(UserCreationForm):
+class CustomWidgetMixin:
+    def add_custom_widget_attrs(self, field_name, placeholder):
+        self.fields[field_name].widget.attrs.update({
+            'placeholder': placeholder,
+            'class': 'w-full py-4 px-6 rounded-xl'
+        })
+
+class LoginForm(AuthenticationForm, CustomWidgetMixin):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_custom_widget_attrs('username', 'Your username')
+        self.add_custom_widget_attrs('password', 'Your password')
+
+class SignupForm(UserCreationForm, CustomWidgetMixin):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
-    username = forms.CharField(widget=forms.TextInput(attrs={
-         'placeholder': 'Your username',
-         'class': 'w-full py-4 px-6 rounded-xl'
-     }))
-    email = forms.CharField(widget=forms.EmailInput(attrs={
-        'placeholder': 'Your email address',
-        'class': 'w-full py-4 px-6 rounded-xl'
-    }))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Your password',
-        'class': 'w-full py-4 px-6 rounded-xl'
-    }))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Repeat password',
-        'class': 'w-full py-4 px-6 rounded-xl'
-    }))
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_custom_widget_attrs('username', 'Your username')
+        self.add_custom_widget_attrs('email', 'Your email address')
+        self.add_custom_widget_attrs('password1', 'Your password')
+        self.add_custom_widget_attrs('password2', 'Repeat password')
